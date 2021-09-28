35903a645ac06795a50b7dbcf8a49f128df860dd
MoElkilany <mohamedsayedelkilany@gmail.com>
2021-09-28 10:35:10 +0200

    Initial Commit

---
+++ HealthKit-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKit-Demo
+//
+//  Created by Mohamed Elkilany on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
